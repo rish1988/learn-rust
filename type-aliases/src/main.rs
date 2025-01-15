ee8c652c6669309b4090c70facbf966da85c48b6
Rishab Dhar <rishab.dhar@magnolia-cms.com>
2025-01-15 16:32:17 +0800

    Type aliases

---
+++ type-aliases/src/main.rs
@@ -0,0 +1,12 @@
+type Meters = i32;
+
+fn main() {
+    let distance: Meters = 100;
+    println!("This is a {distance}m race.");
+
+    let sprint: Meters = 200;
+    println!("This is a {sprint}m sprint.");
+
+    let marathon: Meters = 42195;
+    println!("This is a {marathon}m marathon.");
+}
